2b78235eb24c1f29c1e5f322a14457b442fa5838
Camila <camilaferna140494@gmail.com>
2025-09-18 23:08:02 -0300

    feat: add @tanstack/react-query and @biomejs/biome to project dependencies

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	darkMode: "class", // muito importante! indica que o dark mode vai depender da classe "dark"
+	content: ["./src/**/*.{js,ts,jsx,tsx}"],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+};
