a755096652b81f8eb7f3bcf7b80b337604585826
Camila <camilaferna140494@gmail.com>
2025-09-19 21:48:18 -0300

    refactor: remove OpportunitiesTable and simplify Dashboard layout

--- src/pages/Dashboard.tsx
+++ src/pages/Dashboard.tsx
@@ -1,26 +1,20 @@
 import { useState } from "react";
 import { LeadDetailPanel } from "../components/LeadDetailPanel";
 import { LeadList } from "../components/LeadList";
-import { OpportunitiesTable } from "../components/OpportunitiesTable";
 import type { Lead } from "../types/lead";
 
 export function Dashboard() {
 	const [selectedLead, setSelectedLead] = useState<Lead | null>(null);
 
 	return (
-		<div className="p-6 grid grid-cols-2 gap-6">
-			<div>
-				<LeadList onSelectLead={setSelectedLead} />
-			</div>
-			<div>
-				<OpportunitiesTable />
-			</div>
+		<>
+			<LeadList onSelectLead={setSelectedLead} />
 
 			<LeadDetailPanel
 				lead={selectedLead}
 				onClose={() => setSelectedLead(null)}
 				onSave={() => {}}
 			/>
-		</div>
+		</>
 	);
 }
